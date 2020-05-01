e11af6c72e820b9e19d2e6b4818dded524020b99
Aaron Culp <aaron_culp@anfcorp.com>
2020-05-01 10:09:08 -0400

    added Google Auth component and auth code

---
+++ src/components/GoogleAuth.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export default class GoogleAuth extends Component {
+
+    componentDidMount() {
+        window.gapi.load('client:auth2', () => {
+            window.gapi.client.init({
+                clientId: "750806491404-3nukkblpnrv0vjoo7t1e0ocnct1v29i3.apps.googleusercontent.com",
+                scope: "email"
+            })
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                Google Auth
+            </div>
+        )
+    }
+}
