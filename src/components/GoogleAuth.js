d673f040ee9b3f7c4cccc2cac98ddcfbd8bb9e96
Aaron Culp <aaron_culp@anfcorp.com>
2020-05-04 15:09:11 -0400

    added in working sign in / sign out buttons

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -16,24 +16,45 @@ export default class GoogleAuth extends Component {
         .then(() => {
           this.auth = window.gapi.auth2.getAuthInstance();
           this.setState({ isSignedIn: this.auth.isSignedIn.get() });
-          this.auth.isSignedIn.listen(this.onAuthChange)
+          this.auth.isSignedIn.listen(this.onAuthChange);
         });
     });
   }
 
   onAuthChange = () => {
-      this.setState({ isSignedIn: this.auth.isSignedIn.get() })
-  }
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
 
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
       return <div>I dont know if were signed in</div>;
     } else if (this.state.isSignedIn) {
-      return <div>I am signed In!</div>;
+      return (
+        <button className="ui red google button" onClick={this.signOut}>
+          <i className="google icon" />
+          Sign Out
+        </button>
+      );
     } else {
-      return <div> I am NOT signed in</div>;
+      return (
+        <button className="ui green google button" onClick={this.signIn}>
+          <i className="google icon" />
+          Sign In
+        </button>
+      );
     }
   }
+
+  signIn = () => {
+    this.setState({ isSignedIn: this.auth.signIn() });
+    console.log(this.state.isSignedIn);
+  };
+
+  signOut = () => {
+    this.setState({ isSignedIn: this.auth.signOut() });
+    console.log(this.state.isSignedIn);
+  };
+
   render() {
     return <div>{this.renderAuthButton()}</div>;
   }
