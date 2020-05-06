ab99d2ba9aeb7c3d163e09527cf01346c9638b96
Aaron Culp <aaron_culp@anfcorp.com>
2020-05-06 12:28:07 -0400

    added in reduxForm. added in field components which link to the renderInput function which ties formProps back to the input.

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { Component } from "react";
+import { Field, reduxForm } from "redux-form";
 
-const StreamCreate = () => {
+class StreamCreate extends Component {
+  renderInput({ input }) {
+    return <input {...input} />;
+  }
+
+  render() {
+    console.log(this.props);
     return (
-        <div>
-            
-        </div>
-    )
+      <form>
+        <Field name="title" component={this.renderInput} />
+        <Field name="description" component={this.renderInput} />
+      </form>
+    );
+  }
 }
 
-export default StreamCreate
+export default reduxForm({
+  form: "streamCreate"
+})(StreamCreate);
